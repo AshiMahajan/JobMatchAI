7b062cec8c2c05f1eb1735a27c762e2f9c066d91
Leonopterxy <ashimmahajan1@gmail.com>
2026-07-05 01:56:14 +0530

    Create __init__.py

---
+++ domains/__init__.py
@@ -0,0 +1,7 @@
+from .resume import ResumeProfile
+from .job_description import JobDescription
+from .ats_result import ATSResult
+from .market_analysis import (
+    JobMarketResult,
+    MarketAnalysisResult
+)
